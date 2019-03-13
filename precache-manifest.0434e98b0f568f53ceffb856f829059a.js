9aa53095c7bd1151efc42d6d0140c31c23bfc063
Michal Zet <michal.zadarnowski@gmail.com>
2019-03-13 01:24:21 +0000

    Updates

---
+++ precache-manifest.0434e98b0f568f53ceffb856f829059a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d16c8dc266d1dd206d90ad45c4e4eb6",
+    "url": "/static/media/chuck.2d16c8dc.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fb2cbd106a2e0b32a1cb",
+    "url": "/static/js/main.3168fdfe.chunk.js"
+  },
+  {
+    "revision": "9db96d5913f73ccc7ca3",
+    "url": "/static/js/2.8be81716.chunk.js"
+  },
+  {
+    "revision": "fb2cbd106a2e0b32a1cb",
+    "url": "/static/css/main.6596cb2c.chunk.css"
+  },
+  {
+    "revision": "9a749a1aa23d643945cc43c1d61db10b",
+    "url": "/index.html"
+  }
+];
